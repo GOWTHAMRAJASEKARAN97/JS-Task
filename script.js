31e69858029510f751b0c32f9bd59bc99b5b4102
GOWTHAMRAJASEKARAN97 <gowthamrajasekaran97@gmail.com>
2022-09-01 10:40:00 +0530

    Finding Vowels in String

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
-function short() {
-    let number = document.getElementById('input').value;
-  if (number >= 1000000000) {
-    console.log((number / 1000000000).toFixed(1) + "B");
-    document.getElementById("output").innerHTML = "The Shortform is: "+
-      (number / 1000000000).toFixed(1) + "B";
-  } else if (number >= 1000000) {
-    console.log((number / 1000000).toFixed(1) + "M");
+function vowels() {
+  let str = document.getElementById("input").value;
+  let vowels = /[aeiou]/gi;
+  let result = str.match(vowels);
+  console.log(result);
+  if (result !== null) {
     document.getElementById("output").innerHTML =
-      "The Shortform is: " + (number / 1000000).toFixed(1) + "M";
+      "vowels in the string are : " + result;
   }
-  if (number >= 1000 && number <= 1000000) {
-    console.log((number / 1000).toFixed(1) + "K");
+
+  if (result == null) {
+    console.log("There is no vowels");
     document.getElementById("output").innerHTML =
-      "The Shortform is: " + (number / 1000).toFixed(1) + "K";
-  } else if (number < 1000) {
-    console.log(number / 1000 + "K");
-    document.getElementById("output").innerHTML =
-      "The Shortform is: " + number / 1000 + "K"; 
+      "There is no vowels in the string!";
   }
-  
 }
