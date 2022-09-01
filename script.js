11898e4150b87d90c24578e5b8ed22262cc2277b
GOWTHAMRAJASEKARAN97 <gowthamrajasekaran97@gmail.com>
2022-09-01 10:36:50 +0530

    Shortforming Numbers

--- script.js
+++ script.js
@@ -1,58 +1,22 @@
-const object = {
-  1: {
-    operation: "success",
-    color: "blue",
-    place: "mumbai",
-  },
-  2: {
-    operation: "failed",
-    color: "yellow",
-    place: "chennai",
-  },
-  3: {
-    operation: "success",
-    color: "red",
-    place: "bangalore",
-  },
-  4: {
-    operation: "failed",
-    color: "navy",
-    place: "kolkatta",
-  },
-  5: {
-    operation: "failed",
-    color: "orange",
-    place: "hyderabad",
-  },
-  6: {
-    operation: "success",
-    color: "purple",
-    place: "delhi",
-  },
-};
-
-Object.keys(object).map(function (key, index) {
-  if (object[key].operation === "success") {
-    object[key].color = "green";
-    console.log(object[key]);
-    document.getElementById("output").innerHTML +=
-      "<br>" +
-      "<ol>" +
-      "OPERATION : " +
-      "<b>" +
-      object[key].operation +
-      "</b>" +
-      "</ol>" +
-      "<br>" +
-      "<li>" +
-      "COLOR  : " +
-      object[key].color +
-      "</li>" +
-      "<br>" +
-      "<li>" +
-      "PLACE  : " +
-      object[key].place +
-      "</li>" +
-      "<br><br>";
+function short() {
+    let number = document.getElementById('input').value;
+  if (number >= 1000000000) {
+    console.log((number / 1000000000).toFixed(1) + "B");
+    document.getElementById("output").innerHTML = "The Shortform is: "+
+      (number / 1000000000).toFixed(1) + "B";
+  } else if (number >= 1000000) {
+    console.log((number / 1000000).toFixed(1) + "M");
+    document.getElementById("output").innerHTML =
+      "The Shortform is: " + (number / 1000000).toFixed(1) + "M";
   }
-});
+  if (number >= 1000 && number <= 1000000) {
+    console.log((number / 1000).toFixed(1) + "K");
+    document.getElementById("output").innerHTML =
+      "The Shortform is: " + (number / 1000).toFixed(1) + "K";
+  } else if (number < 1000) {
+    console.log(number / 1000 + "K");
+    document.getElementById("output").innerHTML =
+      "The Shortform is: " + number / 1000 + "K"; 
+  }
+  
+}
