f5b6f6282b0dfba9972f5bf621543407123f8972
GOWTHAMRAJASEKARAN97 <gowthamrajasekaran97@gmail.com>
2022-09-01 10:18:06 +0530

    Mapping Objects

--- REACT TASK/src/Components/store.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import usersReducer from "./UserSlice1";
-import networksReducer from './NetworksSlice'
-export default configureStore({
-  reducer: {
-    users: usersReducer,
-    networks:networksReducer,
-  },
-});
